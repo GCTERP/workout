ced23244ec2261cbd6ca93a33b362c21456710bd
Manoj K <80871895+manojk89@users.noreply.github.com>
2023-01-28 13:31:43 +0530

    Delete JWT_AUTHENTICATION directory

--- JWT_AUTHENTICATION/controllers/dashboard.js
+++
@@ -1,8 +0,0 @@
-exports.getPrivateDashboardData = (req, res, next) => {
-    res.status(200).json({
-        success:true,
-        data : "You got access to private Dashboard data in this route"
-    })
-}
-
-// enter any required code here
