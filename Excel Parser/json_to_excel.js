2ee3eceafe23fde2dcbb944b7e0f2d2579c4a87a
Manoj K <80871895+manojk89@users.noreply.github.com>
2023-01-28 19:45:58 +0530

    Update json_to_excel.js

--- Excel Parser/json_to_excel.js
+++ Excel Parser/json_to_excel.js
@@ -1,42 +1,33 @@
-const xl = require('excel4node');
-const wb = new xl.Workbook();
-const ws = wb.addWorksheet('Worksheet');
+const XLSX = require('xlsx')
 const data = [
- {
-    "name":"Ram",
-    "DOB":"22/01/2001",
-    "email":"ram@gmail.com",
-    "mobile":"1234567890"
- },
- {
-    "name":"Vinay",
-    "DOB":"05/04/2001",
-    "email":"vinay@gmail.com",
-    "mobile":"9807654321"
- }
- 
+    {
+       "name":"Ram",
+       "DOB":"22/01/2001",
+       "email":"ram@gmail.com",
+       "mobile":"1234567890"
+    },
+    {
+       "name":"Vinay",
+       "DOB":"05/04/2001",
+       "email":"vinay@gmail.com",
+       "mobile":"9807654321"
+    }
 ]
-const headingColumnNames = [
-    "Name",
-    "DOB",
-    "Email",
-    "Mobile",
-]
-//Write Column Title in Excel file
+exports.convertJsonToExcel = () =>{
+    //Create a worksheet
+    const workSheet = XLSX.utils.json_to_sheet(data);
+    //Create a workbook and append the worksheet to workbook
+    const workBook = XLSX.utils.book_new();
 
-let headingColumnIndex = 1;
-headingColumnNames.forEach(heading => {
-    ws.cell(1, headingColumnIndex++)
-        .string(heading)
-});
-//Write Data in Excel file
-let rowIndex = 2;
-data.forEach( record => {
-    let columnIndex = 1;
-    Object.keys(record ).forEach(columnName =>{
-        ws.cell(rowIndex,columnIndex++)
-            .string(record [columnName])
-    });
-    rowIndex++;
-}); 
-wb.write('Data.xlsx');//Excel file will be generated 
+    XLSX.utils.book_append_sheet(workBook, workSheet, "data");
+    // Generate buffer
+    XLSX.write(workBook, { bookType: 'xlsx', type: "buffer" })
+    // Binary string
+    XLSX.write(workBook, { bookType: "xlsx", type: "binary" })
+
+    XLSX.writeFile(workBook, "Data.xlsx")
+
+}
+
+
+
