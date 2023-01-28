1bbec446f7f067d46887a5895c381b958d914f8a
Manoj K <80871895+manojk89@users.noreply.github.com>
2023-01-28 15:01:11 +0530

    Updated with the changes...

--- Tablecomponent/Table.js
+++ Tablecomponent/Table.js
@@ -1,4 +1,3 @@
-
 // 1.Import this component
 // 2.Pass data as json format like
 // Pass data as below json format
@@ -31,30 +30,29 @@
 //             "branch": "IT",
 //             "batch": 2019,
 //             "date": "10.34.2323",
-//             "period": 11
+//             "period": 11
 //         }
-//         ]
-//     const getHeadings = () => {
-//         return Object.keys(data[0]);
-//     }        
+//         ]     
 //     return(
 //         <>
-//             <Table theadData={getHeadings()} tbodyData={data}/>
+//             <Table data={data}/>
 //         </>
 //     )
 // }
 // export default Workout;
 
 
-export default function Table({theadData, tbodyData}) {
-    return (
-        <div className="relative p-1.5 w-full inline-block align-middle">
+const Table = ({data}) => (
+        data &&
+        <>
+       
+        <div className="relative p-1.5 w-fit inline-block align-middle">
             <div className=" overflow-hidden overflow-x-auto shadow-md sm:rounded-lg border">
             <table className="min-w-full divide-y divide-gray-200 text-sm text-left sm:rounded-lg">
     
                 <thead className="rounded-t-lg bg-gray-100 text-xs uppercase">
                     <tr className="">
-                        {theadData.map(heading => {
+                        {Object.keys(data[0]).map(heading => {
                             return <th className="px-5 py-3 text-gray-600 text-left text-xs font-semibold  uppercase tracking-wider" key={heading}>{heading}</th>
                             })
                         }
@@ -62,10 +60,10 @@ export default function Table({theadData, tbodyData}) {
                 </thead>
 
                 <tbody className="divide-y divide-gray-200">
-                    {tbodyData.map((row, index) => {
-                        return <tr className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap hover:bg-sky-50" key={index}>
-                            {theadData.map((key, index) => {
-                                return <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap" key={row[key]}>{row[key]}</td>
+                    {data.map((element) => {
+                        return <tr className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap hover:bg-sky-50" key={element._id}>
+                            {Object.keys(data[0]).map((key, index) => {
+                                return <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap" key={element[key]}>{element[key]}</td>
                             })}
                         </tr>;
                         })
@@ -74,5 +72,7 @@ export default function Table({theadData, tbodyData}) {
             </table>
         </div>
     </div>
-    );
-    }
+    </>
+)
+
+export default Table
