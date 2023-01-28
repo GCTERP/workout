54706e25f573a0d0db0cb0de1cfdf54ce731dfd4
Manoj K <80871895+manojk89@users.noreply.github.com>
2023-01-28 19:45:19 +0530

    Update excel_to_json.js

--- Excel Parser/excel_to_json.js
+++ Excel Parser/excel_to_json.js
@@ -2,7 +2,7 @@ const XLSX = require('xlsx')
 
 const Data = "Data.xlsx"
 
-const convertJsonToExcel = () =>{
+exports.convertExcelToJson = () =>{
 //Read Data in Excel file  
 const s =XLSX.readFile(Data);
 const sheetName = s.SheetNames[0]
@@ -11,4 +11,3 @@ const sheetValue = s.Sheets[sheetName];
 const excel =  XLSX.utils.sheet_to_json(sheetValue);
 console.log(excel)
 }
-convertJsonToExcel()
