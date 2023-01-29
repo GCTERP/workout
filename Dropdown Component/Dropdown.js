3c0f2f078742ac8bdb647e3ecb2581c898772f9d
Manoj K <80871895+manojk89@users.noreply.github.com>
2023-01-29 11:39:51 +0530

    Update Dropdown.js

--- Dropdown Component/Dropdown.js
+++ Dropdown Component/Dropdown.js
@@ -1,37 +1,37 @@
 //Import the component
 //Pass data as below json format
-//{
-// "dept" :[
-//     {"id": "1", "name" : "CSE"},
-//   {"id": "2", "name" : "IT"},
-//      {"id": "3", "name" : "EEE"},
-//      {"id": "4", "name" : "EIE"},
-//      {"id": "5", "name" : "MECH"},
-//      {"id": "6", "name" : "IBT"},
-//      {"id": "7", "name" : "PRODUCTION"},
-//      {"id": "8", "name" : "CIVIL-A"},
-//    {"id": "9", "name" : "CIVIL-B"}
-//      
-//  ]
-//  }
-//Import the data from data.json file
+// const title = "Registration"
+//     const options = 
+//         [
+//             {"name" : "CSE"},
+//             {"name" : "IT"},
+//             {"name" : "EEE"},
+//             {"name" : "EIE"},
+//             {"name" : "MECH"},
+//             {"name" : "IBT"},
+//             {"name" : "PRODUCTION"},
+//             {"name" : "CIVIL-A"},
+//             {"name" : "CIVIL-B"}
+//         ]
+    
 
-import React from 'react';
-import data from './data.json';
-const App = () =>
-(
+//     return (
+//         <>
+//         <Dropdown dropdownoptions={options} dropdowntitle={title}/>
+//         {/* <LoaderPrimary /> */}
+//         </>
+//     )
+
+export default function Dropdown({dropdownoptions,dropdowntitle})
+{
+return (
   <>
-   <div className = 'dropdown-comp text-gray-700  pt-1'>
-    <center>
-     <select className= 'length' style = {{width : '10%'}}>     
-      {data.dept.map((dept) => {
-        return <option key={dept.id} value={dept.id}>{dept.name}</option>
+    <label for="dropdownprimay" className="p-2 pb-0 text-lg text-gray-800 font-bold rounded shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed">{dropdowntitle}</label>
+     <select id="dropdown-primary" className= "shadow-md font-medium bg-gray-50 text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2">     
+      {dropdownoptions.map((options) => {
+        return <option className="p-2 bg-blue-50 opacity-5" value={options.name}>{options.name}</option>
       })}
     </select>
-     
-     </center>
-   </div>
    </>
  );
-
-export default App;
+    }
