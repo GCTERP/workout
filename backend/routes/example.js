693a63ab86916e349deeda0351376840b5d2579f
GCTERP <gct.erp.project@gmail.com>
2023-02-07 11:01:32 +0530

    Post Example

--- backend/routes/example.js
+++ backend/routes/example.js
@@ -1,10 +1,13 @@
 import express from "express";
 
-import { getSomething } from "../controllers/ExampleController.js"
+import { getSomething, postSomething } from "../controllers/ExampleController.js"
 
 const router = express.Router()
 
 // GET: Something
 router.get('/', getSomething)
+
+// POST: Something
+router.post('/', postSomething)
 
 export default router;
